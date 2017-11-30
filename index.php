bd4425a4c1e5b4837361784f3b6999490f0d61e9
Mathieu <mathieu@MacBook-Pro-de-Mathieu.local>
2017-11-30 14:28:31 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+function chargerClasse($classname)
+{
+ $entit = __DIR__. '/model/entities/' . $classname.'.php';
+ $manager = __DIR__. '/model/manager/' . $classname.'.php';
+ if(file_exists($entit)){
+   require $entit;
+ }
+ else {
+   require $manager;
+ }
+}
+
+spl_autoload_register('chargerClasse');
